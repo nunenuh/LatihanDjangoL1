8fc500165247f6a8110b19506f96bb01c92e73d8
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2017-09-15 20:42:16 +0800

    new changes

--- transaksi/urls.py
+++ transaksi/urls.py
@@ -5,6 +5,12 @@ urlpatterns = [
     url(r'^(?P<jurnal_id>\d+)/view$', views.ListTransaksiView.as_view(), name='view'),
     
     url(r'^(?P<jurnal_id>\d+)/simpan/$', views.SimpanTransaksiView.as_view(), name='simpan'),
+
+    url(r'^(?P<jurnal_id>\d+)/edit/(?P<transaksi_id>\d+)$', views.EditTransaksiView.as_view(), name='edit'),
+    url(r'^(?P<jurnal_id>\d+)/update/$', views.UpdateTransaksiView.as_view(), name='update'),
+
+    url(r'^(?P<jurnal_id>\d+)/hapus/(?P<transaksi_id>\d+)$', views.HapusTransaksiView.as_view(), name='hapus'),
+
     # url(r'^hapus/(?P<nomer>\d+)$', views.HapusJurnalView.as_view(), name='hapus'),
     # url(r'^edit/(?P<nomer>\d+)$', views.EditJurnalView.as_view(), name='edit'),
     # url(r'^update/$', views.EditJurnalView.as_view(), name='update'),
--- transaksi/views.py
+++ transaksi/views.py
@@ -52,7 +52,7 @@ class SimpanTransaksiView(HarusLogin, View):
             tr.kredit = form.cleaned_data['kredit']
             tr.save()
             
-            return redirect('transaksi:view',jurnal_id=jurnal_id)
+            return redirect('transaksi:view', jurnal_id=jurnal_id)
         else:
             jurnal = Jurnal.objects.get(pk=jurnal_id)
             transaksis = Transaksi.objects.filter(jurnal__id=jurnal_id)
@@ -66,3 +66,72 @@ class SimpanTransaksiView(HarusLogin, View):
             }
 
             return render(request, template, data)
+
+class EditTransaksiView(View):
+    def get(self, request, jurnal_id, transaksi_id):
+        template = 'transaksi/index.html'
+
+        jurnal = Jurnal.objects.get(pk=jurnal_id)
+        transaksi = Transaksi.objects.get(pk=transaksi_id)
+        transaksis = Transaksi.objects.filter(jurnal__id=jurnal_id)
+
+        data_transaksi = {
+            'id': transaksi.id,
+            'tanggal': transaksi.tanggal,
+            'uraian': transaksi.uraian,
+            'debt': transaksi.debt,
+            'kredit': transaksi.kredit
+        }
+
+        form = TransaksiForm(request.POST or None, initial=data_transaksi)
+
+        data = {
+            'jurnal': jurnal,
+            'form': form,
+            'transaksis': transaksis,
+            'form_mode': 'edit'
+        }
+
+        return render(request, template, data)
+
+class UpdateTransaksiView(View):
+    def post(self, request, jurnal_id):
+        template = 'transaksi/index.html'
+
+        form = TransaksiForm(request.POST or None)
+        if form.is_valid():
+            # dari ngambil data
+            # sampai simpan data
+            id = form.cleaned_data['id']
+            jurnal = Jurnal.objects.get(pk=jurnal_id)
+            tr = Transaksi.objects.get(pk=id)
+            tr.jurnal = jurnal
+            tr.tanggal = form.cleaned_data['tanggal']
+            tr.uraian = form.cleaned_data['uraian']
+            tr.debt = form.cleaned_data['debt']
+            tr.kredit = form.cleaned_data['kredit']
+            tr.save(force_update=True)
+            
+            return redirect('transaksi:view', jurnal_id=jurnal_id)
+        else:
+            jurnal = Jurnal.objects.get(pk=jurnal_id)
+            transaksis = Transaksi.objects.filter(jurnal__id=jurnal_id)
+            form = TransaksiForm(request.POST or None)
+
+            data = {
+                'jurnal': jurnal,
+                'transaksis': transaksis, 
+                'form': form,
+                'form_mode': 'edit',
+            }
+
+            return render(request, template, data)
+
+class HapusTransaksiView(View):
+    def get(self, request, jurnal_id, transaksi_id):
+        jurnal = Jurnal.objects.get(pk=jurnal_id)
+        trx = Transaksi.objects.get(jurnal=jurnal, pk=transaksi_id)
+        if trx:
+            trx.delete()
+
+        return redirect('transaksi:view', jurnal_id=jurnal_id)
